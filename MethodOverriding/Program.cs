8a5924b348b955b837ce76b8285b75ec5320a1ad
yeasin habib <yeasinhabib1994@gmail.com>
2018-09-21 00:50:12 +0600

    Add project files.

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
+        }
+    }
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public virtual void Draw()
+        {
+            
+        }
+    }
+
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> Shapes)
+        {
+            foreach(var shape in Shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a triangle");
+        }
+    }
+}
